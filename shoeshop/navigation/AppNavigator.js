7e15b4d068021b6a5462074c04e32ea49cd196c9
YonTDH <tdh1503@gmail.com>
2024-11-28 01:31:49 +0700

    update: AppNavigator.js

--- shoeshop/navigation/AppNavigator.js
+++ shoeshop/navigation/AppNavigator.js
@@ -9,13 +9,16 @@ import ProductDetailScreen from '../screens/ProductDetailScreen';
 import FilterScreen from '../screens/FilterScreen';
 import CartScreen from '../screens/CartScreen';
 import PaymentScreen from '../screens/PaymentScreen';
-import LoginScreen from '../screens/LoginScreen'; // Import màn hình Login
-import RegisterScreen from '../screens/RegisterScreen'; // Import màn hình Register
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import AccountScreen from '../screens/AccountScreen';
+import Menu from '../screens/Menu';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Trạng thái đăng nhập
+  const [user, setUser] = useState(null); // Lưu thông tin người dùng sau khi đăng nhập
 
   return (
     <CartProvider>
@@ -24,55 +27,32 @@ const AppNavigator = () => {
           {/* Điều hướng màn hình khi người dùng chưa đăng nhập */}
           {!isLoggedIn ? (
             <>
-              <Stack.Screen
-                name="Login"
-                options={{ headerShown: false }} // Ẩn header khi ở màn hình Login
-              >
-                {() => <LoginScreen setIsLoggedIn={setIsLoggedIn} />}
+              <Stack.Screen name="Login" options={{ headerShown: false }}>
+                {() => <LoginScreen setIsLoggedIn={setIsLoggedIn} setUser={setUser} />}
               </Stack.Screen>
-              <Stack.Screen
-                name="Register"
-                component={RegisterScreen} // Màn hình đăng ký
-                options={{ title: 'Register' }}
-              />
+              <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Register' }} />
             </>
           ) : (
             <>
               {/* Các màn hình chính sau khi đăng nhập */}
+              <Stack.Screen name="Home" component={AllDealsScreen} options={{ title: 'ShoeShop' }} />
+              <Stack.Screen name="Cart" component={CartScreen} options={{ title: 'Cart' }} />
+              <Stack.Screen name="Payment" component={PaymentScreen} options={{ title: 'Payment' }} />
+              <Stack.Screen name="ProductList" component={ProductListScreen} options={{ title: 'Product List' }} />
+              <Stack.Screen name="ProductDetail" component={ProductDetailScreen} options={{ title: 'Product Details' }} />
+              <Stack.Screen name="Filter" component={FilterScreen} options={{ title: 'Filters' }} />
+              <Stack.Screen name="Checkout" component={CheckoutScreen} options={{ title: 'Checkout' }} />
               <Stack.Screen
-                name="Home"
-                component={AllDealsScreen}
-                options={{ title: 'ShoeShop' }}
+                name="Account"
+                component={AccountScreen}
+                options={{ title: 'Account' }}
+                initialParams={{ user }} // Truyền thông tin người dùng vào AccountScreen
               />
               <Stack.Screen
-                name="Cart"
-                component={CartScreen}
-                options={{ title: 'Cart' }}
-              />
-              <Stack.Screen
-                name="Payment"
-                component={PaymentScreen}
-                options={{ title: 'Payment' }}
-              />
-              <Stack.Screen
-                name="ProductList"
-                component={ProductListScreen}
-                options={{ title: 'Product List' }}
-              />
-              <Stack.Screen
-                name="ProductDetail"
-                component={ProductDetailScreen}
-                options={{ title: 'Product Details' }}
-              />
-              <Stack.Screen
-                name="Filter"
-                component={FilterScreen}
-                options={{ title: 'Filters' }}
-              />
-              <Stack.Screen
-                name="Checkout"
-                component={CheckoutScreen}
-                options={{ title: 'Checkout' }}
+                name="Menu"
+                component={Menu}
+                options={{ title: 'Menu' }}
+                initialParams={{ user }} // Truyền thông tin người dùng vào Menu
               />
             </>
           )}
